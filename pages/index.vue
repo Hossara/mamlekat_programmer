bf9e67b46f10de6388f26e1353ed7cd373e48103
Hossein Araghi <hoseinaraghi84@gmail.com>
2023-05-30 22:10:06 +0330

    Init

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "main",
+})
+</script>
+
+<template>
+  <div id="home">
+    <h1 class="text-blue-400">Hi</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
